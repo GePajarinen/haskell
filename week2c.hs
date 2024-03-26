75801621ee4a13b02229097946825ac2b75e4536
Gesiane Pajarinen <gesianef@hotmail.com>
2024-03-26 19:44:13 +0200

    ex 3

--- week2c.hs
+++ week2c.hs
@@ -7,4 +7,21 @@ evaluates to a list with all the strings of the input list that either begin or 
 Grader sorts the output of your function so the order in which the elements are does not matter.
 
 Submit a file that has the function
+
+------------------------------------------------------------------------------------------------
+Test 0: headOrLast ["hello"] 'o'
+Test 1: headOrLast ["abc","cde","efg"] 'e'
+Cases: 2  Tried: 2  Errors: 0  Failures: 0
+------------------------------------------
+Test 0:
+Test 1:
+Test 2:
+### Failure in: 2: an empty string is given
+
+Test 3:
+Cases: 4  Tried: 4  Errors: 0  Failures: 1
+
 -}
+
+headOrLast :: [String] -> Char -> [String]
+headOrLast list c = [x | x <- list, head x == c || last x == c ] 
